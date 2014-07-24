92076c85fefd0fd97e994afc4fee595b06ddbb08
xiekun <xiekun605746@163.com>
2014-07-24 14:53:35 +0800

    improve demo

--- YMDPickerVIewDemo/ViewController.m
+++ YMDPickerVIewDemo/ViewController.m
@@ -9,11 +9,13 @@
 #import "ViewController.h"
 #import "YMDPickerView.h"
 #import "NSDate+YMDExtensions.h"
-@interface ViewController ()
+@interface ViewController () <YMDPickerViewDelegate>
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    __weak IBOutlet UILabel *_dateLbl;
+}
 
 - (void)viewDidLoad
 {
@@ -26,13 +28,26 @@
                                           shortMonths: NO
                                        numberedMonths: YES
                                            andToolbar: NO];
+    CGPoint origin = datePicker.frame.origin;
+    datePicker.delegate = self;
+    datePicker.frame = (CGRect){origin.x, origin.y+20, datePicker.frame.size};
     [self.view addSubview:datePicker];
+
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma - YMDPickerView
+- (void)pickerDidSelectDate:(NSDate *)date{
+    NSInteger year = [date year];
+    NSInteger month = [date month];
+    NSInteger day = [date day];
+    _dateLbl.text = [NSString stringWithFormat:@"%d/%02d/%d", year, month, day];
 }
 
 @end
